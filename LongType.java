684c14d412f6fe3eebcca9fe9f71df3360406a5b
yagonakano <156016804+yagonakano@users.noreply.github.com>
2024-01-08 17:06:55 -0300

    Add files via upload

---
+++ LongType.java
@@ -0,0 +1,8 @@
+/**Long um arquivo com muitos bites */
+
+public class LongType {
+    public static void main(String[] args){
+        long longVar = 999999999999999999L;
+        System.out.println(longVar);
+    }
+}
