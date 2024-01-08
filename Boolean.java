d7265a03292ebd41e4dfb0b60f1bb0dec85902f3
yagonakano <156016804+yagonakano@users.noreply.github.com>
2024-01-08 16:59:49 -0300

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,8 @@
+public class Boolean {
+    public static void main(String[] arq){
+        boolean VouF = true;
+        System.out.println(VouF);
+        
+    }
+
+}
---
+++ Teste.java
@@ -0,0 +1,10 @@
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println(ImprimirTextoMaiusculo("Hello world" + (1 + 6 * 7)));
+    }
+public static String ImprimirTextoMaiusculo(String Texto){
+    return Texto.toUpperCase();
+} 
+    
+}
